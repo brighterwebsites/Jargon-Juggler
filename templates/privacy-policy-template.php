1ebd24e42a68127e858bea5971cff568ebcf1f65
Vanessa Wood <brighterwebsitedesign@gmail.com>
2024-10-04 13:00:12 +1000

    Create privacy-policy-template.php

---
+++ templates/privacy-policy-template.php
@@ -0,0 +1,7 @@
+<?php
+// privacy-policy-template.php
+// brighterwebsites.com.au/wp-content/plugins/jargon-jugg/templates/privacy-policy-template.php
+//$privacy_policy_content = 'Content for Privacy Policy...';
+
+// privacy-policy-template.php
+return 'Content for Privacy Policy...';   // Update with actual Privacy Policy Text - reduced content durring testing. 
